1eb9e6565b6b1fbfa870aafc8cabd4914e663add
StevenLomon <steven.lennartsson@gmail.com>
2024-11-01 20:28:54 +0100

    Realized that Next.js is a React framework so switched to Vanilla React to start with! Fresh React app with Hello world compiled successfully

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HelloWorld from './components/HelloWorld';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,jsx,ts,tsx}",    // If using the Next.js App Router
-    "./pages/**/*.{js,jsx,ts,tsx}",  // For files in the pages directory
-    "./components/**/*.{js,jsx,ts,tsx}", // Common components directory
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
